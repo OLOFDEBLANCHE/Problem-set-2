0f0b6fa4c5ab7f978b3e3d1d06cba2b4466522ed
OLOFDEBLANCHE <olof.deblanche@gmail.com>
2026-01-29 16:49:48 +0100

    starting on j

--- Problem set 2.R
+++ Problem set 2.R
@@ -5,6 +5,9 @@ library(tidyr)
 
 N = 25
 X = rnorm(mean = 0, sd = 1, n = N)
+test = c(1,2,3)
+
+(test-1)^2
 
 mean = c(0,0,2,2)
 var = c(1/2, 2, 1/2, 2)
@@ -55,3 +58,205 @@ lines(df$val, df$X4, type = "l", col = "black")
 
 legend(-1,2,legend=c("(0,1/2)","(0,2)","(2,1/2)", "(2,2)"), col=c("red","blue","green","black"),
        pch=c("l","l","l","l"))
+
+
+##e-h
+mu = 0
+tao = 1/2 
+alpha = 1
+beta = 1
+
+
+mu_0= c(0,-2)
+sigma_0 = c(1,5)
+
+means = matrix(nrow = 2, ncol = 1001)
+sigmas = matrix(nrow = 2, ncol = 1001)
+
+for (j in 1:2){
+  means[j,1] = mu_0[j]
+  sigmas[j,1] = sigma_0[j]
+  
+  for(i in 1:1000){
+    
+    mu_n = (N/sigmas[j,i]*mean(X)+1/tao*mu)/(N/sigmas[j,i]+1/tao)
+    tao_n = 1/(N/sigmas[j,i]+1/tao)
+    
+    means[j,i+1] = rnorm(n = 1, mean = mu_n, sd = sqrt(tao_n)) 
+    
+    squares = (X - means[j,i+1])^2
+    
+    shape_a = alpha + N/2
+    
+    rate_b = beta + 1/2*sum(squares)
+    
+    sigmas[j,i+1] = 1/rgamma(n = 1,shape = shape_a, rate =rate_b)
+    
+    
+  } 
+  
+  
+
+}
+
+plot(1:1001, means[1,])
+plot(1:1001, means[2,])
+plot(1:1001, sigmas[1,])
+plot(1:1001, sigmas[2,])
+
+#f
+dens_mean = density(means[1,])
+dens_sigmas = density(sigmas[1,])
+
+
+mu_n = (N/1*mean(X)+1/tao*mu)/(N/1+1/tao)
+tao_n = 1/(N/1+1/tao)
+
+
+f_mean = 1/sqrt(2 * pi * tao_n)*exp(-(dens_mean$x-mu_n)^2/(2*tao_n))
+
+plot(dens_mean$x, dens_mean$y, type = "l")
+lines(dens_mean$x, f_mean, type = "l", col = "blue")
+
+plot(dens_sigmas$x, dens_sigmas$y, type = "l")
+
+
+
+#h
+dens2_mean = density(means[2,])
+dens2_sigmas = density(sigmas[2,])
+
+
+f_var = 1/sqrt(2 * pi * tao_n)*exp(-(dens2_mean$x[-1]-mu_n)^2/(2*tao_n))
+
+plot(dens2_mean$x[-1], dens2_mean$y[-1], type = "l")
+lines(dens2_mean$x[-1], f_var, type = "l", col = "blue")
+
+plot(dens2_sigmas$x[-1], dens2_sigmas$y[-1], type = "l")
+
+
+
+
+#i-j
+
+mu_0 = 0
+sigma_0 = 1
+candidate_sd = 2
+
+means = rep(0, times = 1001)
+vars = rep(0, times = 1001)
+
+means[1] = mu_0
+vars[1] = sigma_0
+
+mu_n = (N/1*mean(X)+1/tao*mu)/(N/1+1/tao)
+tao_n = 1/(N/1+1/tao)
+
+squares = (X - mu)^2
+
+shape_a = alpha + N/2
+
+rate_b = beta + 1/2*sum(squares)
+
+for(i in 1:1001){
+  cand_mean = rnorm(n = 1, mean = means[i], sd = candidate_sd)
+  cand_var = rnorm(n = 1, mean = vars[i], sd = candidate_sd)
+  
+  if(cand_var<0){
+    cand_var = cand_var*(-1)
+  }
+  
+  posterior_cand_mean = dnorm(x = cand_mean, mean = mu_n, sd = sqrt(tao_n)) 
+  poserterior_mean = dnorm(x = means[i], mean = mu_n, sd = sqrt(tao_n)) 
+  
+  frac_mean = posterior_cand_mean / poserterior_mean
+  
+  
+  posterior_cand_var = dgamma(x = 1/cand_var, shape = shape_a, rate = rate_b)
+  posterior_var = dgamma(x = 1/vars[i], shape = shape_a, rate = rate_b)
+  
+  frac_var = posterior_cand_var / posterior_var
+  
+  u = runif(n = 1)
+  
+  if(u <= frac_mean){
+    means[i+1] = cand_mean
+  }
+  
+  else{
+    means[i + 1] = means[i]
+  }
+  
+  if(u <= frac_var){
+    vars[i+1] = cand_var
+  }
+  
+  else{
+    vars[i + 1] = vars[i]
+  }
+  
+}
+
+##i
+
+
+dens3_mean = density(means)
+dens3_sigmas = density(sigmas)
+
+
+plot(dens3_mean$x, dens3_mean$y, type = "l")
+lines(dens_mean$x, dens_mean$y, type = "l", col = "blue")
+
+plot(dens_sigmas$x, dens_sigmas$y, type = "l")
+lines(dens3_sigmas$x, dens3_sigmas$y, type = "l", col = "blue")
+
+
+##j 
+
+
+
+
+for(i in 1:1001){
+  cand_mean = rnorm(n = 1, mean = means[i], sd = candidate_sd)
+  cand_var = rnorm(n = 1, mean = vars[i], sd = candidate_sd)
+  
+  if(cand_var<0){
+    cand_var = cand_var*(-1)
+  }
+  
+  posterior_cand_mean = dnorm(x = cand_mean, mean = mu_n, sd = sqrt(tao_n)) 
+  poserterior_mean = dnorm(x = means[i], mean = mu_n, sd = sqrt(tao_n)) 
+  
+  frac_mean = posterior_cand_mean / poserterior_mean
+  
+  
+  posterior_cand_var = dgamma(x = 1/cand_var, shape = shape_a, rate = rate_b)
+  posterior_var = dgamma(x = 1/vars[i], shape = shape_a, rate = rate_b)
+  
+  frac_var = posterior_cand_var / posterior_var
+  
+  u = runif(n = 1)
+  
+  if(u <= frac_mean){
+    means[i+1] = cand_mean
+  }
+  
+  else{
+    means[i + 1] = means[i]
+  }
+  
+  if(u <= frac_var){
+    vars[i+1] = cand_var
+  }
+  
+  else{
+    vars[i + 1] = vars[i]
+  }
+  
+}
+
+
+
+
+
+
